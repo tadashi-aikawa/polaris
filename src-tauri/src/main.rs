588ffe6f3c3b3d4118e79567bfe96aa5ad692795
tadashi-aikawa <syou.maman@gmail.com>
2022-03-30 01:10:56 +0900

    Make a command concurrent

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,13 +4,13 @@
 )]
 
 mod action;
-mod external;
 mod domain;
+mod external;
 
-use std::sync::Mutex;
 use crate::config::Config;
 use crate::domain::entity::User;
 use crate::external::config;
+use std::sync::RwLock;
 
 struct InnerPolarisState {
     config: Config,
@@ -21,54 +21,53 @@ impl InnerPolarisState {
         self.current_user = user;
     }
 }
-struct PolarisState(pub Mutex<InnerPolarisState>);
+struct PolarisState(pub RwLock<InnerPolarisState>);
 
 #[tauri::command]
-fn search_messages(
+async fn search_messages(
     state: tauri::State<'_, PolarisState>,
     query: String,
 ) -> Result<action::search_messages::Response, String> {
     println!("search_messages");
+    let token = state.0.read().unwrap().config.slack_token.clone();
 
-    tauri::async_runtime::block_on(
-        action::search_messages::exec(
-            state.0.lock().unwrap().config.slack_token.as_str(), query
-        )
-    )
+    action::search_messages::exec(&token, query)
+        .await
         .map_err(|e| e.to_string())
 }
 
 #[tauri::command]
-fn initialize(
-    state: tauri::State<'_, PolarisState>,
-) -> Result<(), String> {
-    let current_user  = tauri::async_runtime::block_on(
-        action::get_current_user::exec(
-            state.0.lock().unwrap().config.slack_token.as_str()
-        )
-    )
-        .map(|x| x.user)
-        .map_err(|e| e.to_string())?;
+fn initialize(state: tauri::State<'_, PolarisState>) -> Result<(), String> {
+    let current_user = tauri::async_runtime::block_on(action::get_current_user::exec(
+        state.0.read().unwrap().config.slack_token.as_str(),
+    ))
+    .map(|x| x.user)
+    .map_err(|e| e.to_string())?;
 
-    let mut state_guard = state.0.lock().unwrap();
+    let mut state_guard = state.0.write().unwrap();
     state_guard.set_current_user(Some(current_user));
 
     Ok(())
 }
 
 #[tauri::command]
-fn fetch_queries(
-    state: tauri::State<'_, PolarisState>,
-) -> Vec<String> {
-    state.0.lock().unwrap().config.queries.to_vec()
+fn fetch_queries(state: tauri::State<'_, PolarisState>) -> Vec<String> {
+    state.0.read().unwrap().config.queries.to_vec()
 }
 
 fn main() {
     let config = config::load().unwrap();
 
     tauri::Builder::default()
-        .manage(PolarisState(Mutex::new(InnerPolarisState { config, current_user: None })))
-        .invoke_handler(tauri::generate_handler![initialize, search_messages, fetch_queries])
+        .manage(PolarisState(RwLock::new(InnerPolarisState {
+            config,
+            current_user: None,
+        })))
+        .invoke_handler(tauri::generate_handler![
+            initialize,
+            search_messages,
+            fetch_queries
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
