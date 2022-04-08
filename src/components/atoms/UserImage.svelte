ce4dfb2f45e6f0dba7284e616cb2d7146e67d74d
tadashi-aikawa <syou.maman@gmail.com>
2022-04-09 02:44:10 +0900

    任意パラメータが必須になっていたのを修正

--- src/components/atoms/UserImage.svelte
+++ src/components/atoms/UserImage.svelte
@@ -8,8 +8,8 @@
   import { userMap } from "~/stores";
   import type { Nullable } from "owlelia";
 
-  export let user: Nullable<User>;
-  export let userId: Nullable<string>;
+  export let user: Nullable<User> = null;
+  export let userId: Nullable<string> = null;
   export let size: number = 48;
 
   $: user = user ? user : $userMap[userId];
